0a77aaf7ea141babd88ad8fb9d9cefc113814cc0
Cristea Alexandru <alex.p.cristea@gmail.com>
2025-11-16 20:46:08 +0200

    Added more l;ayout tests according to the test cases; Added method the in the homePage to verify title font size;

--- cypress/e2e/pages/homePage.js
+++ cypress/e2e/pages/homePage.js
@@ -1,10 +1,18 @@
 class HomePage {
   elements = {
     titleText: () => cy.get("h1.home"),
+    logoImage: () => cy.get("img[src*=Logo]"),
   };
 
   verifyTitleIs(expected) {
     this.elements.titleText().should("have.text", expected);
+  }
+
+  verifyTitleFontSize(expectedSize, expectedWeight) {
+    this.elements
+      .titleText()
+      .should("have.css", "font-size", expectedSize)
+      .and("have.css", "font-weight", expectedWeight);
   }
 }
 
--- cypress/e2e/tests/part1Tests/home-layout.cy.js
+++ cypress/e2e/tests/part1Tests/home-layout.cy.js
@@ -1,6 +1,27 @@
 import HomePage from "../../pages/homePage";
 
-it("Testing locators", () => {
+beforeEach(() => {
   cy.visit("https://airportlabs.com");
+});
+
+it("should have the correct test for section title ", () => {
   HomePage.verifyTitleIs("Digital Innovation.Efficient Aviation.");
+});
+
+it("should have the correct font size for section title", () => {
+  HomePage.verifyTitleFontSize("72px", "300");
+});
+
+it("should be visible on desktop", () => {
+  cy.viewport(1280, 720);
+  HomePage.elements.titleText().should("be.visible");
+});
+
+it("should be visible on mobile", () => {
+  cy.viewport(375, 667);
+  HomePage.elements.titleText().should("be.visible");
+});
+
+it("should have visible logo", () => {
+  HomePage.elements.logoImage().should("be.visible");
 });
